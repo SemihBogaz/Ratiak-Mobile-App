30f787c66d5ef06fac38d88f0314f463ce405df5
SemihBogaz <sbogazq@gmail.com>
2020-07-04 23:27:34 +0300

    final state of the app

--- app/src/main/java/com/ratiakapp/ratiak/AddActivity.java
+++ app/src/main/java/com/ratiakapp/ratiak/AddActivity.java
@@ -38,7 +38,7 @@ public class AddActivity extends AppCompatActivity implements GoogleApiClient.Co
 
     //definition for recaptcha
     private GoogleApiClient googleApiClient;
-    private String SITE_KEY="#######";
+    private String SITE_KEY="6LdETgAVAAAAAMBoQE9JSRVXSKEsfJyHCTUO5g4u";
 
     //form definitions
     private TextView Name;
